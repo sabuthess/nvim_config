faa3854383602fae52cf6167929ac72db19a5706
sabuthess <dilan.developer22@gmail.com>
2026-08-08 23:10:39 -0500

    add formating code

---
+++ lua/sabu/plugins/formatting.lua
@@ -0,0 +1,18 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      json = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+  },
+}
