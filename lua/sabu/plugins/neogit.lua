794d0aa1e124694f48af82abf03c3b64cd2d1af8
sabuthess <dilan.developer22@gmail.com>
2026-08-12 11:53:53 -0500

    feat: add neogit plugin

---
+++ lua/sabu/plugins/neogit.lua
@@ -0,0 +1,13 @@
+-- ~/.config/nvim/lua/plugins/neogit.lua
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
+  },
+}
